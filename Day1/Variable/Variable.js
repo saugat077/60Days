e0fac2178c23afb106a2eb91cc26c4951d3e03f6
Saugat K.C <saugatbca2078@Prime.edu.np>
2023-06-01 16:45:41 +0545

    Add files via upload

---
+++ Day1/Variable/Variable.js
@@ -0,0 +1,17 @@
+var fullName = "Saugat K.C";
+
+console.log(fullName);
+
+var isloggedIn = false;
+
+var loggedCount = 34;
+
+console.log(loggedCount);
+
+console.log("loggedCount");
+
+console.log(2+2);
+
+var paymentMode;
+paymentMode="HAHAHAHA"
+console.log(paymentMode);
---
+++ Day1/Variable/Variables.js
@@ -0,0 +1,19 @@
+const uid = "Jiojrtkrw8347"
+var fullName = "Saugat K.C"
+var email = "boomboom@gmial.com"
+var password = "3894723"
+var confirmPassword = "3894723"
+var courseCount =0
+var isLoggedInFromGoogle = false
+
+// fullName = prompt("Enter your name");
+// console.log("Name : "  + fullName);
+
+console.log(`
+
+With unique ID: ${uid}
+User is : ${fullName}
+and his email is : ${email}
+and uses the password: ${password}
+
+`);
